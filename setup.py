f1806b159213a44c3ff45abb3dba8a0d984d8eb8
David Francos Cuartero <yo.orco@gmail.com>
2011-02-07 16:14:53 -0500

    Will not print everything in terminal when using GUI. Changed some things, in an attempt to fix digenpy to work in windows. Thanks to Javier Jarava from RSA for the warning

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-
+import sys
 setup(name='Digenpy',
       version='1.0',
       description='Python default wireless dictionary generators',
       author='David Francos Cuartero (XayOn)',
       author_email='xayon@davidfrancos.net',
       url='http://github.com/XayOn/Digenpy',
-      packages=['Digenpy'],
+      packages=['Digenpy_'],
       scripts=['digenpy', 'digenpy-gtk'],
-      data_files=[('/usr/share/', ['digenpy.ui'])],
+      data_files=[(sys.prefix + '/share/', ['digenpy.ui'])],
      )
