bbe4b5fe474ab7c070be2ca426f7a888b3e315b9
David Francos Cuartero <yo.orco@gmail.com>
2011-02-10 12:40:34 +0100

    Ok, I know this is kinda stupid, but adding binaries here until I find another place and while I experiment with the setup in windows

--- setup.py
+++ setup.py
@@ -13,14 +13,15 @@ if os.name is not "posix":
 	
 opts = {
     "py2exe": {
-        "excludes": "pango,atk,gobject",
+        'includes': 'cairo, pango, pangocairo, atk, gobject, gio',
+        "excludes": "atk,gdk",
         "dll_excludes": [
         "iconv.dll","intl.dll","libatk-1.0-0.dll",
         "libgdk_pixbuf-2.0-0.dll","libgdk-win32-2.0-0.dll",
         "libglib-2.0-0.dll","libgmodule-2.0-0.dll",
-        "libgobject-2.0-0.dll","libgthread-2.0-0.dll",
         "libgtk-win32-2.0-0.dll","libpango-1.0-0.dll",
         "libpangowin32-1.0-0.dll"],
+        'packages': ['Digenpy_'],
         }
     }
 
