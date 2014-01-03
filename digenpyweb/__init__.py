8bd418486ce676160121951f02a1d2bea2ccbd25
David Francos Cuartero <work@davidfrancos.net>
2014-01-03 14:07:59 +0100

    New digenpy web

--- digenpyweb/__init__.py
+++ digenpyweb/__init__.py
@@ -1,28 +1,46 @@
-from flask import Flask, render_template, abort, Response, request
+from flask import Flask, render_template, abort, Response, request, redirect, flash, url_for
 import Digenpy_, json
 from Digenpy_ import *
 app = Flask(__name__)
-app.config['DEBUG'] = False
+app.config['DEBUG'] = True
+app.secret_key = "SECRETKEY"
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/get/<country>/<company>/<mac>/<essid>')
+
+
 def do_digenpy(country, company, mac, essid):
-    method = request.environ['REQUEST_METHOD']
-    result = getattr(getattr(Digenpy_, country),
-        company)(['', mac, essid]).dictionary
+    error = False
+    try:
+        result = getattr(getattr(Digenpy_, country),
+            company)(['', mac, essid]).dictionary
+    except Exception, err:
+        result = err
+        error = True
+
     if not result:
         abort(404)
-    if method == "POST":
+
+    return (result, error)
+
+@app.route('/get/<country>/<company>/<mac>/<essid>')
+def ajax_file(country, company, mac, essid):
+    result, error = do_digenpy(country, company, mac, essid)
+    if error:
+        flash(result)
+        return redirect(url_for('index'))
+    if request.environ['REQUEST_METHOD'] == "POST":
         return json.dumps(result)
-    elif method == "GET":
-        return '\n'.join(result)
+    return '\n'.join(result)
 
 @app.route('/get_file/<country>/<company>/<mac>/<essid>')
 def download_file(country, company, mac, essid):
-    return Response(do_digenpy(country, company, mac, essid),
-        mimetype='application/x-digenpy-dic' )
+    result, error = do_digenpy(country, company, mac, essid)
+    if error:
+        flash(result)
+        return redirect(url_for('index'))
+    return Response(result, mimetype='application/x-digenpy-dic' )
 
 def server():
     """ Main server, will allow us to make it wsgi'able """
--- digenpyweb/static/js/script.js
+++ digenpyweb/static/js/script.js
@@ -1,17 +1,9 @@
-/* Author:
-    David Francos Cuartero
-    GPL 2+
-*/
-
-function get_digenpy_url(){
-	if($('#WPA').is(':checked')){
-		return "/get/Spanish/" + $('#company').val() + "WPA/" + $('#mac').val() + "/" + $('#essid').val();
-    }
-	return "/get_file/Spanish/" + $('#company').val() + "/" + $('#mac').val() + "/" + $('#essid').val();
-}
-
-function do_digenpy(){
-    $('#result').attr('src',get_digenpy_url());
-}
-
-
+window.do_digenpy = () ->
+    if ($ 'input[name=WPA]').is(':checked')
+        jQuery.ajax "/get/Spanish/" + ($ '#company').val() + "WPA/" + ($ '#mac').val() + "/" + ($ '#essid').val(),
+            type: 'GET',
+            success: (data, textStatus, jqXHR) ->
+                ($ '#wpa_pass').text(data)
+                ($ '#wpamodal').modal('show')
+    else
+        window.location.href = "/get_file/Spanish/" + $('#company').val() + "/" + $('#mac').val() + "/" + $('#essid').val()
