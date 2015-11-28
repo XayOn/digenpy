9415b2a75b1255e0900274020f8cd0b9c28f3933
David Francos Cuartero <dfrancos@buguroo.com>
2015-11-28 21:43:06 +0100

    Complete rewrite

--- setup.py
+++ setup.py
@@ -3,35 +3,10 @@
 from distutils.core import setup
 import sys, os, shutil
 
-scripts=['digenpy', 'digenpy-gtk']
-if os.name is not "posix":
-    if os.name is "nt":
-        import py2exe
-    shutil.copyfile('digenpy','digenpy.py')
-    shutil.copyfile('digenpy-gtk','digenpy-gtk.py')
-    shutil.copyfile('digenpy-gtk','digenpy-gtk.py')
-    scripts=['digenpy.py', 'digenpy-gtk.py']
-
-opts = {
-    "py2exe": {
-        'includes': 'cairo, pango, pangocairo, atk, gobject, gio',
-        "excludes": "atk,gdk",
-        "dll_excludes": [
-        "iconv.dll","intl.dll","libatk-1.0-0.dll",
-        "libgdk_pixbuf-2.0-0.dll","libgdk-win32-2.0-0.dll",
-        "libglib-2.0-0.dll","libgmodule-2.0-0.dll",
-        "libgtk-win32-2.0-0.dll","libpango-1.0-0.dll",
-        "libpangowin32-1.0-0.dll"],
-        'packages': ['Digenpy_'],
-        }
-    }
-
 setup(name='Digenpy',
       version='1.4.1',
       description='Python default wireless dictionary generators',
       author='David Francos Cuartero',
-      windows = [{"script": "digenpy-gtk.py" }],
-      console = [{"script": "digenpy.py" }],
       author_email='me@davidfrancos.net',
       url='http://github.com/XayOn/Digenpy',
       download_url='http://github.com/XayOn/Digenpy',
@@ -44,10 +19,16 @@ setup(name='Digenpy',
       mantainer='David Francos Cuartero (XayOn)',
       mantainer_email='xayon@xayon.net',
       long_description="Default password dictionary generator for multiple companys",
-      packages=['Digenpy_'],
-      scripts=scripts,
-      options=opts,
+      packages=['digenpy'],
       package_data={
           'Digenpy_': ['Digenpy.ui'],
       },
-     )
+      entry_points={
+          'console_scripts': [
+              'dgenpy = digenpy.main:cli',
+          ],
+          'gui_scripts': [
+              'digenpy-gtk = digenpy.main:gui'
+          ]
+      }
+)
