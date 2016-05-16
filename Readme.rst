b519f56c96e4f7874f8e4705639da1827232a30b
David Francos Cuartero <dfrancos@buguroo.com>
2016-05-17 00:38:59 +0200

    readme

--- Readme.rst
+++ Readme.rst
@@ -1,1 +1,9 @@
-doc/Readme.rst
+About Digenpy
+--------------
+Digenpy is a set of dictionary generators written in python.
+It's extremely modular and easily extensible.
+
+Usage
+------
+
+.. image:: example.png
--- setup.py
+++ setup.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python
-# For some reason, building exes with py2exe doesnt work right now.
+"""
+    Digenpy
+"""
 from distutils.core import setup
-import sys, os, shutil
 
 setup(name='Digenpy',
-      version='1.4.1',
+      version='2.0.0',
       description='Python default wireless dictionary generators',
       author='David Francos Cuartero',
       author_email='me@davidfrancos.net',
       url='http://github.com/XayOn/Digenpy',
       download_url='http://github.com/XayOn/Digenpy',
       license='GPL2',
-      requires=['pygtk','gettext'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
       ],
       mantainer='David Francos Cuartero (XayOn)',
       mantainer_email='xayon@xayon.net',
-      long_description="Default password dictionary generator for multiple companys",
+      long_description=("Default wifi password dictionary generator"
+                        " for multiple companys"),
       packages=['digenpy'],
-      package_data={
-          'Digenpy_': ['Digenpy.ui'],
-      },
       entry_points={
           'console_scripts': [
-              'dgenpy = digenpy.main:cli',
+              'digenpy = digenpy:main',
           ],
-          'gui_scripts': [
-              'digenpy-gtk = digenpy.main:gui'
-          ]
-      }
-)
+      })
